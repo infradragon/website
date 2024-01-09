abdd7f3db808b8743fcb3961971670b13a384c18
infradragon <72665124+infradragon@users.noreply.github.com>
2024-01-09 07:58:15 -0600

    Update navbar-includer.js

--- navbar-includer.js
+++ navbar-includer.js
@@ -1,22 +1,3 @@
-function includeNavbar() {
-  var z, i, elmnt, file, xhttp;
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    file = elmnt.getAttribute("include-navbar");
-    if (file) {
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Unable to load navbar. idfk.";}
-          elmnt.removeAttribute("include-navbar");
-          includeNavbar();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      return;
-    }
-  }
-}
+fetch('static/navbar.html')
+  .then(t = t.text()).then(t => document.querySelector("head > nav").innerHTML = t)
+  .catch(_ => document.querySelector("head > nav").textContent = "Unable to load navbar. idfk.")
