46f0f30e37e81a985f9c6c01adce12a16dfaa803
infradragon <72665124+infradragon@users.noreply.github.com>
2024-01-09 08:00:07 -0600

    Update navbar-includer.js

--- navbar-includer.js
+++ navbar-includer.js
@@ -1,3 +1,5 @@
+function includeNavbar() {
 fetch('static/navbar.html')
   .then(t = t.text()).then(t => document.querySelector("head > nav").innerHTML = t)
   .catch(_ => document.querySelector("head > nav").textContent = "Unable to load navbar. idfk.")
+}
